7cedfbf264a3ab6a1dd94050d7b81804354a87f8
Chandler Moisen <chandler@percolate.com>
2016-11-08 22:32:49 -0500

    update class 12

--- materials/class12/class12/list-maker/js/app.js
+++ materials/class12/class12/list-maker/js/app.js
@@ -6,9 +6,9 @@
 
 // Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called when #clickme is clicked and appends the <li> to #list
 
-// Bonus: Focus on #item after the item is added
-// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user
-// Legendary Bonus: Remove individual <li> elements when they are clicked
+// Bonus: Focus on #item after the item is added (hint: look up "jquery focus")
+// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (hint: use an if/else statement)
+// Legendary Bonus: Remove individual <li> elements when they are clicked (hint: use $(this) and .remove())
 
 $(document).ready(function () {
 
