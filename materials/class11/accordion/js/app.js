7a195415a1a5c493bbf2dba665aff9f96a9fce77
Chandler Moisen <chandler@percolate.com>
2017-04-09 11:47:59 -0400

    update class 11

---
+++ materials/class11/accordion/js/app.js
@@ -0,0 +1,6 @@
+// The following can be written in 3 lines of jQuery/JavaScript!
+
+// 1. Add a .click() handler to elements with the class of .row
+// 2. Associate this .click() handler with an anoymous function (in other words, "pass" .click() and anonymous function)
+// 3. Inside the body of the anonymous function, use the concept of $(this) to .find() the <div class="wrapper">
+// 4. Once you find the .wrapper, use the .slideToggle() function to trigger the desired behavior
