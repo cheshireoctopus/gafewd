a09df47f8bee8bbc57c1da753b37230c2ab8304f
Chandler Moisen <chandlermoisen@gmail.com>
2017-10-28 14:48:08 -0400

    udpate and surface class 12

--- materials/class12/click-count/js/app.js
+++ materials/class12/click-count/js/app.js
@@ -2,10 +2,10 @@
 //
 // 1: User clicks on #clickme
 // 2: Increment the value of #click-num by one for each click (eg if clicknum is 0, make it 1; it clicknum is 10, make it 11; etc.)
-// 3: If the number of clicks == 5, change the background-color of <body> to red
-// 4: Else if the number of clicks == 10, change the background-color of <body> to green
-// 5: Else if the number of clicks == 15, change the background-color of <body> to blue
-// 6: Else, change the background-color of <body> to black
+// 3: If the number of clicks === 5, change the background of <body> to red
+// 4: Else if the number of clicks === 10, change the background of <body> to green
+// 5: Else if the number of clicks === 15, change the background of <body> to blue
+// 6: Else, change the background of <body> to black
 
 $('#clickme').onClick(clickCount);
 
