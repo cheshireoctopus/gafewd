193606214cb8efaf0ef0ffa368d02233c0dcdff0
Chandler Moisen <chandlermoisen@gmail.com>
2017-10-29 10:44:32 -0400

    update class 13

--- materials/class13/list-maker/js/app.js
+++ materials/class13/list-maker/js/app.js
@@ -6,16 +6,14 @@
 
 // Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called when #clickme is clicked and appends the <li> to #list
 
-// Bonus: Focus on #item after the item is added
-// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user
-// Legendary Bonus: Remove individual <li> elements when they are clicked
+// Bonus: Focus on #item after the item is added (research jQuery focus method)
+// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (if/else conditions)
+// Legendary Bonus: Remove individual <li> elements when they are clicked (will have to use $(this) + research how to remove dynamically added elements)
 
-$(document).ready(function () {
-
-	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
-	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
-	// 1B. This anonymous function should call a separate function, appendItem, and pass it the variable newItem
-	// 1C. This anonymous function should then clear the text inside of #item
-
-	// 2. Write the appendItem function here
-})
+// To Get Started:
+// 1. Attach a .click() event to #clickme
+// 2. This .click() should call a function, named handleClick
+// 3. handleClick should create a variable, named newItem, that is equal to the value of #item
+// 4. handleClick should call a separate function, named appendItem, and pass it the variable newItem
+// 5. handleClick should then clear the text inside of #item
+// 6. Finally, write the function, appendItem (see above)
