4bf27dc2ae5eb26fcb420a868531f53bba151298
Chandler Moisen <chandlermoisen@gmail.com>
2017-11-06 17:38:03 -0500

    class 14 hw fix

--- materials/class14/cash-register/js/app.js
+++ materials/class14/cash-register/js/app.js
@@ -4,11 +4,11 @@
 // 3: #total is updated to reflect the sum of all the #entries
 //
 // Instructions:
-// 1: Add a .submit event to the form; use an anonymous function to handle the sumbit
-// Inside the anonymous function:
-// 2: Remeber to prevent the default action on the form!
-// 3: Create a variable to store the user input from #newEntry
-// 4: Remember to call parseFloat() on this variable! It needs to be a number.
-// 5: Append a new <tr></tr> to the <tbody id="entries"> containing two <td>'s, one of which contains the new variable: <tr><td></td><td>NEW VALUE</td></tr>
-// 6: Figure out a way to update the number in #total (Hint: look back at the calculator box example)
-// 7: Empty the input in #newEntry (clear it out)
+// 1: Add a .submit event to the form
+// 2. Associate the above submit event with a function, handleSubmit, which should do the following:
+// 3: Remeber to prevent the default action on the form - hint: event.preventDefault()
+// 4: Create a variable to store the user input from #newEntry
+// 5: Remember to call parseFloat() on this variable! It needs to be a number.
+// 6: Append a new <tr></tr> to the <tbody id="entries"> containing two <td>'s, one of which contains the new variable: <tr><td></td><td>NEW VALUE</td></tr>
+// 7: Figure out a way to update the number in #total (Hint: look back at the calculator box example)
+// 8: Empty the input in #newEntry (clear it out)
