75b2637c0c753d06be8fb0fcdde6074b420f5f5a
cheshireoctopus <chandlermoisen@gmail.com>
2018-10-28 20:48:21 -0400

    update class 13

---
+++ materials/class13/calculator-box/js/script.js
@@ -0,0 +1,21 @@
+ /*
+	
+	Add .click() handlers for each of the boxes:
+
+	A) Boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
+
+	B) Boxes #m10, #m20, and #m30 should each subtract 10, 20, and 30, respectively, from the value in the center box, #out
+
+	C) #red and #blue should change the background color of #out to red and blue, respectively
+	
+	D) #out should change the background of #out to white, and set the value back to zero
+
+	HINT: define a variable, var count = 0 outside of any functions. Use this variable to keep track of what to display inside of #out
+	Ex: when the user clicks #a10, add 10 to the "count" and then reflect this change in the HTML
+
+	jQuery Methods Needed:
+	- .click()
+	- .html()
+	- .css()
+
+*/
--- materials/class14/calculator-box-solution/js/script.js
+++ materials/class14/calculator-box-solution/js/script.js
@@ -1,6 +1,6 @@
  /*
 	
-	To begin, add .click() handlers for each of the boxes:
+	Add .click() handlers for each of the boxes:
 
 	A) Boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
 
@@ -10,8 +10,8 @@
 	
 	D) #out should change the background of #out to white, and set the value back to zero
 
-	HINT: define a variable, var count = 0, at the top of your function; use this variable to keep track of what to display inside of #out
-	Ex: when the user clicks #a10, add 10 to the var count, and then reflect this change in the HTML
+	HINT: define a variable, var count = 0 outside of any functions. Use this variable to keep track of what to display inside of #out
+	Ex: when the user clicks #a10, add 10 to the "count" and then reflect this change in the HTML
 
 	jQuery Methods Needed:
 	- .click()
