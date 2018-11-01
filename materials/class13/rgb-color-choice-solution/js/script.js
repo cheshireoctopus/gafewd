f31ae672b3f28e5a54be0acbb6a789ca794e5ef3
cheshireoctopus <chandlermoisen@gmail.com>
2018-11-01 15:10:22 -0400

    materials cleaning

--- materials/class13/rgb-color-choice-solution/js/script.js
+++ materials/class13/rgb-color-choice-solution/js/script.js
@@ -1,18 +1,27 @@
-// Create click event handler
-document.querySelector('#colorbutton').onclick = changeColor;
+/*
+  Project:
+    RGB Color Choice
 
-function changeColor () {
-	// assign variables
-	var red = document.querySelector('#red').value;
-	var green = document.querySelector('#green').value;
-	var blue = document.querySelector('#blue').value;
+  Desired Functionality:
+    1. User enters three values into each respective input: #red, #green, #blue
+    2. User clicks "Change the Color!" button, afterwhich steps 3 and 4 occur
+    3. The content within #colorful-text needs to update to reflect the user's input
+      ex: If the user enters: 143, 56, 200 then #colorful-text should contain: rgb(143, 56, 200)
+    4. The background color of #wrapper changes to the rgb-value the user entered
+      ex: If the user enters: 143, 56, 200 then the background of #wrapper should be rgb(143, 56, 200)
+  Hints:
+    1. You will have to rely on string concatenation to build the rgb-value given the user's input: 'rgb(' + ...
+*/
 
-	// create rgb string from variables
-	var rgbStr = "rgb(" + red + "," + green + "," + blue + ")";
+function changeTheColor() {
+  var red = document.querySelector('#red').value
+  var green = document.querySelector('#green').value
+  var blue = document.querySelector('#blue').value
 
-	// change innerHTML of colorfultext element
-	document.querySelector('#colorfultext').innerHTML = rgbStr;
+  var rgbValue = 'rgb(' + red + ',' + green + ',' + blue + ')'
 
-	// change the body's background color
-	document.querySelector('body').style.backgroundColor = rgbStr;
+  document.querySelector('#colorful-text').innerHTML = rgbValue
+  document.querySelector('#wrapper').style.background = rgbValue
 }
+
+document.querySelector('#color-button').onclick = changeTheColor
