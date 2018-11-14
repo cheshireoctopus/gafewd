fd173223b179664badddc3ff545584420c6d27cc
cheshireoctopus <chandlermoisen@gmail.com>
2018-11-13 21:00:31 -0500

    class 17 update

--- materials/class17/citi-pix/js/app.js
+++ materials/class17/citi-pix/js/app.js
@@ -1,13 +1,15 @@
-// User flow:
-// 1: User changes <select id="city-type">
-// 2: The page's background changes to a picture of the newly selected city
+/*
+  User flow:
+    1: User changes <select id="city-type">
+    2: The page's background changes to a picture of the newly selected city
 
-// Instructions:
-// 1. Attach the .change() event method to #city-type;
-// 2. Associate the above change event with a function, handleChange, which should do the following:
-// 3. Create a variable, city, that stores the selected value of #city-type (hint: use .val())
-// 4. Remove all classes from the <body> element using the .removeClass() method
-// 5. The variable you created in step 4, corresponds to a CSS class (check in the sytle.css file for more info)
-//    Use the .addClass() method to add this variable as a class to <body>
+  Instructions:
+    1. Attach the .change() event method to #city-type
+    2. Associate the above change event with a function, handleChange, which should do the following:
+    3. Create a variable, city, that stores the selected value of #city-type (hint: use .val())
+    4. Remove all classes from the <body> element using the .removeClass() method
+    5. The variable you created in step 4, corresponds to a CSS class (check in the style.css file for more info)
+       Use the .addClass() method to add this variable as a class to <body>
 
-// Bonus: Apply the concept of $(this) - think about what element is causing .change() to fire
+  Bonus: Apply the concept of $(this) - think about what element is causing .change() to fire
+*/
