1f9e5a144179ad47dbff670af5d1d81439594bb6
cheshireoctopus <chandlermoisen@gmail.com>
2018-11-13 20:04:52 -0500

    add citi-pix-solution

---
+++ materials/class18/citi-pix-solution/js/app.js
@@ -0,0 +1,27 @@
+/*
+  User flow:
+    1: User changes <select id="city-type">
+    2: The page's background changes to a picture of the newly selected city
+
+  Instructions:
+    1. Attach the .change() event method to #city-type
+    2. Associate the above change event with a function, handleChange, which should do the following:
+    3. Create a variable, city, that stores the selected value of #city-type (hint: use .val())
+    4. Remove all classes from the <body> element using the .removeClass() method
+    5. The variable you created in step 4, corresponds to a CSS class (check in the style.css file for more info)
+       Use the .addClass() method to add this variable as a class to <body>
+
+  Bonus: Apply the concept of $(this) - think about what element is causing .change() to fire
+*/
+
+$('#city-type').change(handleChange)
+
+function handleChange() {
+  var city = $('#city-type').val()
+
+  // refactored using concept of this
+  // var city = $(this).val()
+
+  $('body').removeClass().addClass(city)
+}
+
